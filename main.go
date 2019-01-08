2b8dab7a5dc5f0d7bcf10df834a719b52341c32d
BigmouthFringe <bigmouthfringe@gmail.com>
2019-01-08 17:07:07 +0700

    Fix async connections

--- main.go
+++ main.go
@@ -10,18 +10,82 @@ import (
 	"net"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func listenToClient(address string) {
 	fmt.Println("Launching server...")
+
 	// Listen to all interfaces
-	ln, _ := net.Listen("tcp", address)
-	// Accept connections
-	conn, _ := ln.Accept()
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	var prot *protector.Protector
 	for {
+		// Accept connections
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		go handleConnection(conn, prot)
+	}
+}
+
+func dialToServer(port string) {
+	conn, err := net.Dial("tcp", port)
+	for conn == nil || err != nil {
+		fmt.Println("Dialing failed with error", err)
+
+		fmt.Println("Retrying to connect")
+		conn, err = net.Dial("tcp", port)
+	}
+	fmt.Println("Dialing to " + port, conn)
+
+
+	var initialSKey = getSessionKey()
+	var initialHash = getHashString()
+
+	var sKey string
+	var prot = protector.New(initialHash)
+	for i := 0; i < 3; i++ {
+		var keyAndHash = initialSKey + ":" + initialHash
+		if sKey == "" {
+			fmt.Println("Text to send: " + keyAndHash)
+			fmt.Fprintf(conn, keyAndHash + "\n")
+			sKey = initialSKey
+		} else {
+			sKey = prot.GenerateNextSessionKey(sKey)
+			fmt.Println("Text to send: " + sKey)
+			fmt.Fprintf(conn, sKey + "\n")
+		}
+
 		message, _ := bufio.NewReader(conn).ReadString('\n')
+		message = message[:len(message) - 1] // Remove '\n' sign
+		fmt.Println("Message from server: " + message)
+
+		sKey = prot.GenerateNextSessionKey(sKey)
+		if message != sKey {
+			panic("Keys don't match")
+			break
+		}
+		fmt.Println("Keys are equal")
+	}
+
+	conn.Close()
+}
+
+func handleConnection(conn net.Conn, prot *protector.Protector) {
+	for {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		if &message == nil || len(message) == 0 {
+			fmt.Println("Connection was closed by client")
+			conn.Close()
+			return
+		}
+
 		message = message[:len(message) - 1] // Remove '\n' sign
 		fmt.Println("Message Received: " + string(message))
 
@@ -37,43 +101,7 @@ func listenToClient(address string) {
 		}
 
 		var sKey = prot.GenerateNextSessionKey(clientSKey)
-		conn.Write([]byte(sKey + "\n"))
-	}
-}
-
-func dialToServer(port string) {
-	fmt.Println("Dialing to " + port)
-	conn, _ := net.Dial("tcp", port)
-
-
-	var initialSKey = getSessionKey()
-	var initialHash = getHashString()
-
-	var sKey string
-	var prot = protector.New(initialHash)
-	for {
-		var keyAndHash = initialSKey + ":" + initialHash
-		if sKey == "" {
-			fmt.Println("Text to send: " + keyAndHash)
-			fmt.Fprintf(conn, keyAndHash + "\n")
-			sKey = initialSKey
-		} else {
-			fmt.Println("Text to send: " + sKey)
-			fmt.Fprintf(conn, sKey + "\n")
-		}
-
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		message = message[:len(message) - 1] // Remove '\n' sign
-		fmt.Println("Message from server: " + message)
-
-		sKey = prot.GenerateNextSessionKey(sKey)
-		if message != sKey {
-			panic("Keys don't match")
-			break
-		}
-		fmt.Println("Keys are equal")
-
-		sKey = prot.GenerateNextSessionKey(sKey)
+		fmt.Println("Text to send: " + sKey)
 		fmt.Fprintf(conn, sKey + "\n")
 	}
 }
@@ -140,14 +168,17 @@ func main() {
 	}
 
 	if mode == "server" {
-		for i := 0; i < int(*instQuantityPtr); i++ {
-			listenToClient(*portPtr)
-		}
+		listenToClient(*portPtr)
 	}
 
 	if mode == "client" {
+		var wg sync.WaitGroup
 		for i := 0; i < int(*instQuantityPtr); i++ {
-			dialToServer(*portPtr)
+			wg.Add(1)
+			go dialToServer(*portPtr)
 		}
+		wg.Wait()
 	}
+
+	fmt.Println("Program exited :(")
 }
